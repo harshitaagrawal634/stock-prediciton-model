9b05f5f2fba3125c5e7a928110f54efedac2902b
harshita <harshita.agrawal634@gmail.com>
2025-06-23 14:32:29 +0530

    updated file

--- pages/utils/plotly_figure.py
+++ pages/utils/plotly_figure.py
@@ -9,14 +9,14 @@ def plotly_table(dataframe):
     rowOddColor='#e1efff'
     fig=go.Figure(data=[go.Table(
     header=dict(
-        values=["<b><b>"]+["<b>"+str(i)[:10]+"<b>" for i in dataframe.columns],
-        line_color='#0078ff',fill_color='#0078ff',
-        align='center',font=dict(color='white',size=15),height=35,        
+        values=["<b>Index</b>"]+["<b>"+str(i)[:10]+"</b>" for i in dataframe.columns],
+        line_color='#0078ff', fill_color='#0078ff',
+        align='center', font=dict(color='white', size=15), height=35,        
     ),
     cells=dict(
-        values=[["<b>"+str(i)+"<b>" for i in dataframe.index]]+[dataframe[i] for i in dataframe.columns],
-        fill_color=[[rowOddColor,rowEvenColor]],
-        align='left',line_color=['white'],font=dict(color=["black"],size=15)
+        values=[[str(i) for i in dataframe.index]] + [dataframe[i].tolist() for i in dataframe.columns],
+        fill_color=[ [rowOddColor if i%2 else rowEvenColor for i in range(len(dataframe))] ] * (len(dataframe.columns)+1),
+        align='left', line_color='white', font=dict(color="black", size=15)
     )
     )])
     fig.update_layout(height=350, margin=dict(l=0,r=0,t=0,b=0))
